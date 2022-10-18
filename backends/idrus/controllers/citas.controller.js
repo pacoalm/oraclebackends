c9906a2ac9ab0ccb68a98f70fdcc4c61903bb806
pacoalm <pacoalm@gmail.com>
2022-10-18 15:38:20 +0100

    1810H

--- backends/idrus/controllers/citas.controller.js
+++ backends/idrus/controllers/citas.controller.js
@@ -1,5 +1,5 @@
 import oracledb from 'oracledb';
-import moment from 'moment'
+import moment from 'moment';
 
 var oracle =  require('../database/connection.js')
 
@@ -15,19 +15,19 @@ function fechaValida(fecha) {
     console.log(dia + '/' + mes + '/' + eje)
 
     var date = moment(fecha,'YYYYMMDD');
-    console.log(date);
+    
     if (date.isValid()) return true;
 
     return false;
 
 }
 
-export const getCitas = async(req, res, next) =>{
+export const getCitasCEX = async(req, res, next) =>{
    
     //Validar fecha 
  
     var fecha = req.query.fecha;
-    console.log(fecha)
+  
 
     if (fecha ===undefined || !fechaValida(fecha)) {
         res.writeHead(500, {'Content-Type': 'application/json'});
@@ -42,8 +42,15 @@ export const getCitas = async(req, res, next) =>{
         const eje = fecha.substr(0,4);
         const mes = fecha.substr(4,2);
         const dia = fecha.substr(6,2);
-        const fechacita = '18/10/2022'
-        const fechacitamasuno = '19/10/2022'
+        
+        const fechacita = dia + '/' + mes + '/' + eje;
+        var fechacitamasuno;
+
+    
+        var d = new Date(eje + '-' + mes + '-' + dia)
+        d.setDate(d.getDate() + 1)
+        fechacitamasuno = ('0' + d.getDate()).slice(-2) + '/' +  ('0' + (d.getMonth() + 1)).slice(-2) + '/' + d.getFullYear();
+        
 
         const pool = await oracle.getConnection();
         pool.execute("SELECT * FROM V07_Idrus_cex_dias WHERE fecha_cita > to_date(:fechacita,'DD/MM/YYYY') AND fecha_cita < to_date(:fechacitamasuno,'DD/MM/YYYY')" ,[fechacita, fechacitamasuno], {outFormat: oracledb.OBJECT}, 
--- backends/idrus/routes/citas.routes.js
+++ backends/idrus/routes/citas.routes.js
@@ -1,14 +1,14 @@
 
 import auth from "./auth";
 
-import {getCitas, getCamas} from '../controllers/citas.controller'
+import {getCitasCEX} from '../controllers/citas.controller'
 
 var express = require ('express');
 
 const router = express.Router();
 
 
-router.get('/api/citas/', auth(),  getCitas);
+router.get('/api/citas/', auth(),  getCitasCEX);
 
 
 
