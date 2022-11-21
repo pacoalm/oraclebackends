4cd37e5104e219977e131d3730c0f15be6e73704
pacoalm <pacoalm@gmail.com>
2022-11-21 15:29:18 +0000

    2111H

--- backends/idrus/controllers/encuestas.controller.js
+++ backends/idrus/controllers/encuestas.controller.js
@@ -1,7 +1,29 @@
 import oracledb from 'oracledb';
+import moment from 'moment';
 
 
 var oracle =  require('../database/connection.js')
+
+
+function fechaValida(fecha) {
+
+    if(fecha.length !== 8) {
+        return false
+    }
+    const eje = fecha.substr(0,4);
+    const mes = fecha.substr(4,2);
+    const dia = fecha.substr(6,2);
+
+    console.log(dia + '/' + mes + '/' + eje)
+
+    var date = moment(fecha,'YYYYMMDD');
+    
+    if (date.isValid()) return true;
+
+    return false;
+
+}
+
 
 
 export const getEncuestasHosp = async(req, res, next) =>{
@@ -15,7 +37,7 @@ export const getEncuestasHosp = async(req, res, next) =>{
             res.writeHead(500, {'Content-Type': 'application/json'});
             res.end(JSON.stringify({
                 status:500,
-                message:"Error al recuperar muestra.",
+                message:"Error al recuperar muestra HOSP.",
                 detailed_message: err.message
             }));
         }
@@ -28,3 +50,152 @@ export const getEncuestasHosp = async(req, res, next) =>{
     );
 } 
 
+
+export const getEncuestasCex = async(req, res, next) =>{
+   
+    
+    const pool = await oracle.getConnection();
+    pool.execute("SELECT * FROM V07_Idrus_Encuestas_Cex",[], {outFormat: oracledb.OBJECT}, 
+    function(err,result) {
+    if (err) {
+        console.log ('Error el ejecutar la query:' + err.message)
+        res.writeHead(500, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({
+            status:500,
+            message:"Error al recuperar muestra CEX.",
+            detailed_message: err.message
+        }));
+    }
+    else {
+        
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(result.rows));
+    }
+}
+);
+} 
+
+export const getEncuestasTraReh = async(req, res, next) =>{
+   
+    
+    const pool = await oracle.getConnection();
+    pool.execute("SELECT * FROM V07_Idrus_Encuestas_TraReh",[], {outFormat: oracledb.OBJECT}, 
+    function(err,result) {
+    if (err) {
+        console.log ('Error el ejecutar la query:' + err.message)
+        res.writeHead(500, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({
+            status:500,
+            message:"Error al recuperar muestra Tra-Reh.",
+            detailed_message: err.message
+        }));
+    }
+    else {
+        
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(result.rows));
+    }
+}
+);
+} 
+
+export const getEncuestasPruebas = async(req, res, next) =>{
+   
+    
+    const pool = await oracle.getConnection();
+    pool.execute("SELECT * FROM V07_Idrus_Encuestas_Pruebas",[], {outFormat: oracledb.OBJECT}, 
+    function(err,result) {
+    if (err) {
+        console.log ('Error el ejecutar la query:' + err.message)
+        res.writeHead(500, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({
+            status:500,
+            message:"Error al recuperar muestra Pruebas.",
+            detailed_message: err.message
+        }));
+    }
+    else {
+        
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(result.rows));
+    }
+}
+);
+} 
+
+export const getEncuestasUrg = async(req, res, next) =>{
+   
+    
+    const pool = await oracle.getConnection();
+    pool.execute("SELECT * FROM V07_Idrus_Encuestas_Urg",[], {outFormat: oracledb.OBJECT}, 
+    function(err,result) {
+    if (err) {
+        console.log ('Error el ejecutar la query:' + err.message)
+        res.writeHead(500, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({
+            status:500,
+            message:"Error al recuperar muestra Urg.",
+            detailed_message: err.message
+        }));
+    }
+    else {
+        
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(result.rows));
+    }
+}
+);
+} 
+
+export const getEncuestasCalVida = async(req, res, next) =>{
+   
+      //Validar fecha 
+ 
+      var fecha = req.query.fecha;
+  
+
+      if (fecha ===undefined || !fechaValida(fecha)) {
+          res.writeHead(500, {'Content-Type': 'application/json'});
+              res.end(JSON.stringify({
+                  status:500,
+                  message:"Fecha Erronea",
+                  detailed_message: "Introduzca una fecha válida (YYYYMMDD)"
+              }));
+          
+      } else {
+          
+          const eje = fecha.substr(0,4);
+          const mes = fecha.substr(4,2);
+          const dia = fecha.substr(6,2);
+          
+          const fechacita = dia + '/' + mes + '/' + eje;
+          var fechacitamasuno;
+  
+      
+          var d = new Date(eje + '-' + mes + '-' + dia)
+          d.setDate(d.getDate() + 1)
+          fechacitamasuno = ('0' + d.getDate()).slice(-2) + '/' +  ('0' + (d.getMonth() + 1)).slice(-2) + '/' + d.getFullYear();
+          
+  
+          const pool = await oracle.getConnection();
+          pool.execute("SELECT * FROM V07_Idrus_Encuestas_CalVida WHERE finterv > to_date(:fechacita,'DD/MM/YYYY') AND finterv < to_date(:fechacitamasuno,'DD/MM/YYYY')" ,[fechacita, fechacitamasuno], {outFormat: oracledb.OBJECT}, 
+          function(err,result) {
+          if (err) {
+              console.log ('Error el ejecutar la query:' + err.message)
+              res.writeHead(500, {'Content-Type': 'application/json'});
+              res.end(JSON.stringify({
+                  status:500,
+                  message:"Error private things",
+                  detailed_message: err.message
+              }));
+          }
+          else {
+              
+              res.writeHead(200, {'Content-Type': 'application/json'});
+              res.end(JSON.stringify(result.rows));
+          }
+      }
+      );
+  } 
+    
+} 
--- backends/idrus/routes/encuestas.routes.js
+++ backends/idrus/routes/encuestas.routes.js
@@ -1,15 +1,18 @@
 
 import auth from "./auth";
 
-import {getEncuestasHosp} from '../controllers/encuestas.controller'
+import {getEncuestasHosp, getEncuestasCex, getEncuestasTraReh, getEncuestasPruebas, getEncuestasUrg, getEncuestasCalVida} from '../controllers/encuestas.controller'
 
 var express = require ('express');
 
 const router = express.Router();
 
-
-router.get('/api/idrus/encuestas/Hosp', auth(),  getEncuestasHosp);
-
+router.get('/api/idrus/encuestas/hosp', auth(),  getEncuestasHosp);
+router.get('/api/idrus/encuestas/cex', auth(),  getEncuestasCex);
+router.get('/api/idrus/encuestas/trareh', auth(),  getEncuestasTraReh);
+router.get('/api/idrus/encuestas/pruebas', auth(),  getEncuestasPruebas);
+router.get('/api/idrus/encuestas/urg', auth(),  getEncuestasUrg);
+router.get('/api/idrus/encuestas/calvida/', auth(),  getEncuestasCalVida);
 
 export default router;
 
