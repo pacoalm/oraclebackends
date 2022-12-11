98de287adff61e3212c0b64d25ccb1f0778e4d8f
pacoalm <pacoalm@gmail.com>
2022-12-11 18:03:13 +0000

    1112Home

--- backends/planificacionqui/controllers/pq.controller.js
+++ backends/planificacionqui/controllers/pq.controller.js
@@ -1,15 +1,16 @@
 
 import { getConnection } from '../database/connection';
+import {v4 as uuidv4} from 'uuid';
+
 var oracledb = require("oracledb");
 
 export const getUbicaciones = async (req, res) => {
 
     const pool = await getConnection();
     var sql="";
-
+    console.log(req.params.facility);
     
-    sql= "SELECT * FROM com_personal where sid =" + req.params.sid
-    
+    sql= "SELECT * FROM  V07_PQ_ubicaciones where facility =" + req.params.facility;
     console.log(sql);
 
     pool
@@ -20,7 +21,7 @@ export const getUbicaciones = async (req, res) => {
                 res.writeHead(500, {'Content-Type': 'application/json'});
                 res.end(JSON.stringify({
                     status:500,
-                    message:"Error recuperando médico",
+                    message:"Error recuperando ubicaciones quirúrgicas",
                     detailed_message: err.message
                 }));
             }
@@ -61,5 +62,42 @@ export const getTiposUbicacion = async (req, res) => {
             }
         }
         )
+}
+
+export const postUbicacion = async (req, res) => {
+
+    console.log('Insertar ubicación');
+    console.log(req.body);
+
+    const pool = await getConnection();
+
+
+   
+    var body = req.body;
+
+    if (body.descripcion && body.alias && body.tipo & body.activa && body.facility) {
+        var sql = "INSERT INTO pq_ubicaciones (uuid, facility, descripcion, alias, tipo, activa_sn) VALUES('" + uuidv4() + "'," + body.facility +
+                  ",'" + body.descripcion + "','" + body.alias + "'," + body.tipo + ",'" + (body.activa===0? 'N':'S') + "')"
+    }
+    else {
+        res.status(500).send('Error al insertar ubicación. Faltan campos en la cabecera');
+        return;
+    }
+        
+    console.log(sql);
+    pool.execute (sql, [],
+    function (err, result) {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Error al insertar ubicación');
+            return;
+                }
+        res.status(200).send('Inserción correcta');
+            return;
+
+    })
+   
+    
+   
 }
 
--- backends/planificacionqui/routes/pq.routes.js
+++ backends/planificacionqui/routes/pq.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import {getTiposUbicacion, getUbicaciones } from "../controllers/pq.controller";
+import {getTiposUbicacion, getUbicaciones, postUbicacion } from "../controllers/pq.controller";
 
 const router = Router();
 
-router.get('/api/ubicaciones', getUbicaciones);
+router.get('/api/ubicaciones/:facility', getUbicaciones);
 router.get('/api/tiposubicacion', getTiposUbicacion);
+router.post('/api/ubicacion', postUbicacion);
 
 
 export default router;
